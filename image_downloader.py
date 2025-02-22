3752be14c32cd8c1c27c2bb39e3e09c5442bba52
sudhakar6295 <53999168+sudhakar6295@users.noreply.github.com>
2025-02-22 10:49:32 +0530

    Code clean up

--- image_downloader.py
+++ image_downloader.py
@@ -13,7 +13,8 @@ logger = logging.getLogger(__name__)
 
 DATABASE_URI = 'mariadb+mariadbconnector://scrape:Nnk0okMJqXcbZxt9@170.239.84.29/scrape' # Update with your credentials
 # Directory to save images
-BASE_DIR = "./downloaded_images"
+IMG_BASE_DIR = "./downloaded_images"
+PDF_BASE_DIR = "./downloaded_pdf"
 
 # Function to download an image
 def download_image(url, save_path):
@@ -30,6 +31,66 @@ def download_image(url, save_path):
     except Exception as e:
         logger.error(f"Failed to download {url}: {e}")
 
+def process_images(images_lst, img_table_dir, table_name):
+ 
+        if isinstance(images_lst, str):
+            try:
+                images_lst = json.loads(images_lst)
+            except json.JSONDecodeError:
+                logger.warning(f"Invalid format in table {table_name}, skipping row: {images_lst}")
+                return
+        
+        if isinstance(images_lst, list):
+            for index, url in enumerate(images_lst):
+                if table_name == 'xag':
+                        image_name = f"{url.split('=')[-1]}.jpg"  # Replace `id` with your primary key column name
+                elif table_name.lower() == 'xsf':
+                    image_url = url
+                    image_name1 = image_url.split('/')[-1].replace('.jpg','')
+                    image_part2 = image_url.split('/')[-2].split('-')[0]
+                    updated_image_name = f"{image_name1}_{image_part2}.jpg"
+                    image_name = updated_image_name
+                else:
+                    image_name = url.split('/')[-1]  # Replace `id` with your primary key column name
+                save_path = os.path.join(img_table_dir, image_name)
+                download_image(url, save_path)
+
+def process_pdf(pdf_lst, pdf_table_dir, table_name):
+
+    if table_name == 'xag':
+        return
+        
+    import pdb; pdb.set_trace()
+
+def process_files(table_name,rows,columns):
+    # Create a directory for the table
+    img_table_dir = os.path.join(IMG_BASE_DIR, table_name)
+    os.makedirs(img_table_dir, exist_ok=True)
+
+    pdf_table_dir = os.path.join(PDF_BASE_DIR, table_name)
+    os.makedirs(pdf_table_dir, exist_ok=True)
+
+    for row in rows:
+        images_lst = row[columns.index('images_url')] 
+        if isinstance(images_lst, str):
+            try:
+                images_lst = json.loads(images_lst)
+            except json.JSONDecodeError:
+                logger.warning(f"Invalid format in table {table_name}, skipping row: {images}")
+
+        if images_lst:
+            process_images(images_lst, img_table_dir, table_name)
+
+        pdf_lst = row[columns.index('pdf')] 
+        if isinstance(pdf_lst, str):
+            try:
+                pdf_lst = json.loads(pdf_lst)
+            except json.JSONDecodeError:
+                logger.warning(f"Invalid format in table {table_name}, skipping row: {pdf_lst}")
+        if pdf_lst:
+            process_pdf(pdf_lst, pdf_table_dir, table_name)
+
+
 # Function to process a table
 def process_table(engine, table_name):
     try:
@@ -41,43 +102,12 @@ def process_table(engine, table_name):
                 logger.info(f"No data found in table {table_name}")
                 return
 
-            result = connection.execute(text(f"SELECT * FROM `{table_name}`")) 
             columns = list(result.keys())
-            if 'images_url' not in columns:
+            if 'images_url' not in columns and 'pdf' not in columns:
                 return
-            
-            # Create a directory for the table
-            table_dir = os.path.join(BASE_DIR, table_name)
-            os.makedirs(table_dir, exist_ok=True)
-            
-            # Iterate through rows and download images
-            for row in rows:
-                images_lst = row[columns.index('images_url')] 
-                if not images_lst:
-                    continue
+
+            process_files(table_name,rows,columns)
                 
-                # Convert images string to a list if needed
-                if isinstance(images_lst, str):
-                    try:
-                        image_urls = json.loads(images_lst)
-                    except json.JSONDecodeError:
-                        logger.warning(f"Invalid format in table {table_name}, skipping row: {images_lst}")
-                        continue
-                
-                if isinstance(image_urls, list):
-                    for index, url in enumerate(image_urls):
-                        if table_name == 'xag':
-                             image_name = f"{url.split('=')[-1]}.jpg"  # Replace `id` with your primary key column name
-                        elif table_name.lower() == 'xsf':
-                            image_url = url
-                            image_name1 = image_url.split('/')[-1].replace('.jpg','')
-                            image_part2 = image_url.split('/')[-2].split('-')[0]
-                            updated_image_name = f"{image_name1}_{image_part2}.jpg"
-                            image_name = updated_image_name
-                        else:
-                            image_name = url.split('/')[-1]  # Replace `id` with your primary key column name
-                        save_path = os.path.join(table_dir, image_name)
-                        download_image(url, save_path)
     except SQLAlchemyError as e:
         logger.error(f"Error processing table {table_name}: {e}")
 
@@ -98,12 +128,16 @@ def main():
     try:
         # Get all table names
         tables = inspector.get_table_names()
+        tables_to_ignore = ['requests_log','sessions']
+
         if not tables:
             logger.info("No tables found in the database.")
             return
         
         # Iterate through each table
         for table_name in tables:
+            if table_name in tables_to_ignore:
+                continue    
             logger.info(f"Processing table: {table_name}")
             process_table(engine, table_name)
     except SQLAlchemyError as e:
